88ae41f4296aff62ffd89d7bbbcbbff43a1fad3d
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-16 00:33:28 +0530

    feat(*): node start

---
+++ index.ts
@@ -0,0 +1,21 @@
+import multer from 'multer';
+import express from 'express';
+import Server from './modules/Server';
+import cors from 'cors';
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+const includeMulter = multer().any();
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    includeMulter(req, res, next)
+});
+
+app.use(express.static('public'));
+
+new Server(app, {
+    PORT: PORT,
+    methodsPath: './methods',
+    autoHandle: true
+});
