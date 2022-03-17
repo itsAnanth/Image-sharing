a8b03d1a69e950327106cf395c1318e1616f6a1b
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-17 15:43:35 +0530

    feat(*): mongoose model

---
+++ database/models/media.model.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+
+const ImageSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        default: crypto.randomBytes(16).toString('hex')
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    image: {
+        type: Buffer
+    },
+    contentType: {
+        type: String,
+        default: 'img/png'
+    }
+});
+
+export default ImageSchema;
