52e2702fa2801d74288827d61a88f0b2e2f76207
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-18 23:31:35 +0530

    chore(REST): code cleanup

--- methods/GET/image.ts
+++ methods/GET/image.ts
@@ -1,19 +1,22 @@
 import Endpoint from "../../modules/Endpoint";
 import db from '../../database/main';
-import stream from 'stream';
-import fs from 'fs';
+import Response from "../../modules/Response";
 
 export default new Endpoint({
     path: '/image',
-    callback: async(req, res, next) => {
+    callback: async (req, res, next) => {
         const id = req.query.id;
         const image = await db.utils.getImage(id as string);
-        if (!image) return console.log('no img found');
+        if (!image)
+            return res.status(Response.status.NOT_FOUND).send(Response.error({
+                message: 'Image not found',
+                code: Response.status.NOT_FOUND
+            }))
         const base64 = Buffer.from(image.buffer).toString('base64');
         console.log(image);
 
-        const index_file = 
-        `<html lang="en">
+        const index_file =
+            `<html lang="en">
         <body>
             <img width="30px" height="30px" src="data:image/png;base64,${base64}" alt="img">
             
