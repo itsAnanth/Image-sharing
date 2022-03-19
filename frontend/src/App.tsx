72ee984afd03a28500dfd7047dcae6a036d926d1
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-19 21:52:12 +0530

    feat(*): client side routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Stream from './components/Stream';
 import Upload from './components/Upload';
+import './css/main.css';
 
 function App() {
 	return (
-		<Upload />
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Upload />}></Route>
+				<Route path='image/:id' element={<Stream />}></Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
